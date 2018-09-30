f982b1bebaeef00ccf2a2658815ee36b991b862c
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2018-09-30 12:27:09 -0400

    start pushing to indiv. branches now

--- src/Server/run_servers.sh
+++ src/Server/run_servers.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #TODO: SPECIFY THE HOSTNAMES OF 4 CS MACHINES (lab1-1, cs-2, etc...)
-MACHINES=(oclark2@lab2-15.cs.mcgill.ca oclark2@lab2-17.cs.mcgill.ca oclark2@lab2-19.cs.mcgill.ca oclark2@lab2-21.cs.mcgill.ca oclark2@lab2-23.cs.mcgill.ca)
+MACHINES=(lab2-15.cs.mcgill.ca lab2-17.cs.mcgill.ca lab2-19.cs.mcgill.ca lab2-21.cs.mcgill.ca lab2-23.cs.mcgill.ca)
+USER="oclark2@"
 
 tmux new-session \; \
 	split-window -h \; \
@@ -10,12 +11,12 @@ tmux new-session \; \
 	split-window -v \; \
 	select-layout main-vertical \; \
 	select-pane -t 0 \; \
-	send-keys "ssh -t ${MACHINES[0]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; ./run_server.sh Flights\"" C-m \; \
+	send-keys "ssh -t $USER${MACHINES[0]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; ./run_server.sh Flights\"" C-m \; \
 	select-pane -t 1 \; \
-	send-keys "ssh -t ${MACHINES[1]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd;   ./run_server.sh Cars\"" C-m \; \
+	send-keys "ssh -t $USER${MACHINES[1]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd;   ./run_server.sh Cars\"" C-m \; \
 	select-pane -t 2 \; \
-	send-keys "ssh -t ${MACHINES[2]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; ./run_server.sh Rooms\"" C-m \; \
+	send-keys "ssh -t $USER${MACHINES[2]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; ./run_server.sh Rooms\"" C-m \; \
 	select-pane -t 3 \; \
-	send-keys "ssh -t ${MACHINES[3]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; ./run_server.sh Rooms\"" C-m \; \
+	send-keys "ssh -t $USER${MACHINES[3]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; ./run_server.sh Customers\"" C-m \; \
 	select-pane -t 4 \; \
-	send-keys "ssh -t ${MACHINES[4]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; sleep .5s; ./run_middleware.sh ${MACHINES[0]} ${MACHINES[1]} ${MACHINES[2]} ${MACHINES[3]}\"" C-m \;
+	send-keys "ssh -t $USER${MACHINES[4]} \"cd Comp100000000Project/src/Server/ > /dev/null; echo -n 'Connected to '; hostname; ls -l; pwd; sleep .5s; ./run_middleware.sh ${MACHINES[0]} ${MACHINES[1]} ${MACHINES[2]} ${MACHINES[3]}\"" C-m \;
