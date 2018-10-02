fab5d663e3c320d5e60a3542568ed1f1c0893fa1
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2018-10-01 22:54:04 -0400

    port

--- src/Server/Server/RMI/TCPResourceManager.java
+++ src/Server/Server/RMI/TCPResourceManager.java
@@ -14,7 +14,7 @@ import java.net.Socket;
 
 public class TCPResourceManager extends ResourceManager {
     private static String serverName = "Server";
-    private static int serverPort = 800;
+    private static int serverPort = 1234;
 
     //private static String s_rmiPrefix = "group30";
 
