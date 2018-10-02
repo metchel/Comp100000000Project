d5c4da69561907c71923f925f048d938d92facdc
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2018-10-02 00:37:31 -0400

    making progress

--- src/Server/Server/Middleware/TCPMiddlewareManager.java
+++ src/Server/Server/Middleware/TCPMiddlewareManager.java
@@ -8,22 +8,57 @@ import java.net.*;
 import java.io.*;
 
 
-public class TCPMiddlewareManager extends Middleware {
+public class TCPMiddlewareManager extends MiddlewareAlt {
 
     private static String s_serverName = "Middleware";
 
     //private static String s_rmiPrefix = "group30";
 
-    //private static String[] serverNames = new String[]{"lab2-15.cs.mcgill.ca","lab2-17.cs.mcgill.ca","lab2-19.cs.mcgill.ca","lab2-21.cs.mcgill.ca"};
+    private static String[] serverNames = new String[4];
 
     private static String[] rmNames = new String[]{"Flights","Cars","Rooms","Customers"};
 
     private static String s_serverHost = "localhost";
-    private static int serverPort = 1099;
+    private static int serverPort = 1235;
+    Socket carRM;
+    Socket flightRM;
+    Socket hotelRM;
+    Socket customerRM;
+    BufferedReader carIn;
+    BufferedReader flightIn;
+    BufferedReader hotelIn;
+    BufferedReader cusstomerIn;
+    PrintWriter carOut;
+    PrintWriter flightOut;
+    PrintWriter hotelOut;
+    PrintWriter customerOut;
+
 
 
     public static void main(String args[])
     {
+
+        for (int i = 0; i < 4; i++){
+            rmNames[i] = args[i];
+        }
+
+        if (System.getSecurityManager() == null)
+        {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            TCPMiddlewareManager mwm = new TCPMiddlewareManager("Middleware");
+            mwm.connectToRMs();
+            //mwm.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+
+
         try {
             ServerSocket server = new ServerSocket(serverPort);
             while (true) {
@@ -36,6 +71,32 @@ public class TCPMiddlewareManager extends Middleware {
         }
 
 
+    }
+
+
+
+    
+    public void connectToRMs(){
+        try {
+            carRM = new Socket(rmNames[0], serverPort);
+            flightRM = new Socket(rmNames[1], serverPort);
+            hotelRM = new Socket(rmNames[2], serverPort);
+            customerRM = new Socket(rmNames[3], serverPort);
+            System.out.println("Connected to " + carRM.getInetAddress()+","+flightRM.getInetAddress()+","+hotelRM.getInetAddress()+","+customerRM.getInetAddress());
+        }catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        carIn = new BufferedReader(new InputStreamReader(carRM.getInputStream()));
+        flightIn = new BufferedReader(new InputStreamReader(flightRM.getInputStream()));
+        hotelIn = new BufferedReader(new InputStreamReader(hotelRM.getInputStream()));
+        customerIn = new BufferedReader(new InputStreamReader(customerRM.getInputStream()));
+
+        carOut = new PrintWriter(carRM.getOutputStream(),true);
+        flightOut = new PrintWriter(flightRM.getOutputStream(),true);
+        hotelOut = new PrintWriter(hotelRM.getOutputStream(),true);
+        customerOut = new PrintWriter(customerRM.getOutputStream(),true);
     }
 
     public TCPMiddlewareManager(String name){
