475db24c27b8e27287989076ca9417e4b48c9000
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2018-10-03 16:46:17 -0400

    change port

--- src/Server/Server/RMI/TCPResourceManager.java
+++ src/Server/Server/RMI/TCPResourceManager.java
@@ -14,7 +14,7 @@ import java.net.Socket;
 
 public class TCPResourceManager extends ResourceManager {
     private static String serverName = "Server";
-    private static int serverPort = 1290;
+    private static int serverPort = 1300;
 
     //private static String s_rmiPrefix = "group30";
 
