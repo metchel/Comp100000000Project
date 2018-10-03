202313f22daaf5c085d2696a96d593d67e35a146
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2018-10-03 16:16:06 -0400

    woe

--- src/Server/Server/Middleware/TCPMiddlewareManager.java
+++ src/Server/Server/Middleware/TCPMiddlewareManager.java
@@ -19,7 +19,7 @@ public class TCPMiddlewareManager  {
     private static String[] rmNames = new String[]{"Flights","Cars","Rooms","Customers"};
 
     private static String s_serverHost = "localhost";
-    private static int serverPort = 1252;
+    private static int serverPort = 1275;
     Socket carRM;
     Socket flightRM;
     Socket hotelRM;
--- src/Server/Server/RMI/TCPResourceManager.java
+++ src/Server/Server/RMI/TCPResourceManager.java
@@ -14,7 +14,7 @@ import java.net.Socket;
 
 public class TCPResourceManager extends ResourceManager {
     private static String serverName = "Server";
-    private static int serverPort = 1252;
+    private static int serverPort = 1275;
 
     //private static String s_rmiPrefix = "group30";
 
