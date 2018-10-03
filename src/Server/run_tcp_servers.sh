a3988be4e2006b9132e9c89540fbe2c6959add11
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2018-10-03 14:41:34 -0400

    rage

--- src/Server/run_tcp_servers.sh
+++ src/Server/run_tcp_servers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #TODO: SPECIFY THE HOSTNAMES OF 4 CS MACHINES (lab1-1, cs-2, etc...)
-MACHINES=(cs-1.cs.mcgill.ca cs-2.cs.mcgill.ca cs-3.cs.mcgill.ca cs-4.cs.mcgill.ca cs-5.cs.mcgill.ca)
+MACHINES=(lab2-14.cs.mcgill.ca lab2-15.cs.mcgill.ca lab2-17.cs.mcgill.ca lab2-18.cs.mcgill.ca lab2-19.cs.mcgill.ca)
 USER="oclark2@"
 tmux new-session \; \
 	split-window -h \; \
