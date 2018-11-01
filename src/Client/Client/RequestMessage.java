6c8d26234e741232c1aeb36dd28ba46503b9a452
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2018-10-31 23:21:21 -0400

    RequestMessage and Builder

--- src/Client/Client/RequestMessage.java
+++ src/Client/Client/RequestMessage.java
@@ -9,6 +9,10 @@ public class RequestMessage {
     private RequestMessage(){
 
     }
+    public String toString(){
+        String tmp = String.join(",", this.argums);
+        return (sName+","+cName+","+xid+","+tmp);
+    }
         
     public static class RequestBuilder {
         private String sName;
@@ -38,7 +42,7 @@ public class RequestMessage {
             }
             else if (command.startsWith("Query")){
                 if (command.length < 13) {
-                    this.cName = "QUERY"
+                    this.cName = "QUERY";
                     switch(command.substring(5,6){
                         case("Cu"): this.sName = "CUSTOMERS"; break;
                         case("Ca"): this.sName = "CARS"; break;
@@ -47,7 +51,7 @@ public class RequestMessage {
                     }
                 }
                 else {
-                    this.cName = "QUERYPRICE"
+                    this.cName = "QUERYPRICE";
                     switch(command.substring(5,6){
                         case("Ca"): this.sName = "CARS"; break;
                         case("Fl"): this.sName = "FLIGHTS"; break;
@@ -57,14 +61,20 @@ public class RequestMessage {
 
             }
             else if (command.startsWith("Reserve")){
+                this.cName = "RESERVE";
+                switch(command.substring(7,8){
+                    case("Ca"): this.sName = "CARS"; break;
+                    case("Fl"): this.sName = "FLIGHTS"; break;
+                    case("Ro"): this.sName = "ROOMS"; break;
+                }
+
 
             }
             else if (command.startsWith("Bundle")){
-
+                this.cName = "BUNDLE";
+                this.sName = "";
             }
 
-            this.sName = sName;
-            this.cName = cName;
             return this;
         }
         public Builder inXId(String xid){
