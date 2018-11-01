74927182f1a36948447746810849522d5b2683f0
metchel <matthew.etchells@mail.mcgill.ca>
2018-11-01 03:38:46 -0400

    ...

--- src/Server/Middleware/MiddlewareRequestHandler.java
+++ src/Server/Middleware/MiddlewareRequestHandler.java
@@ -1,46 +1,50 @@
 package Server.Middleware;
 
+import Server.Network.Request;
+import Server.Network.Response;
+
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 import Server.Sockets.RequestHandler;
-import Server.Network.*;
 
 public class MiddlewareRequestHandler implements RequestHandler {
     final Socket customerClient;
     final Socket flightClient;
     final Socket carClient;
     final Socket roomClient;
-    final BufferedReader customerIn;
-    final PrintWriter customerOut;
-    final BufferedReader flightIn;
-    final PrintWriter flightOut;
-    final PrintWriter carOut;
-    final BufferedReader carIn;
-    final PrintWriter roomOut;
-    final BufferedReader roomIn;
+    final ObjectOutputStream flightOos;
+    final ObjectInputStream flightOis;
+    final ObjectOutputStream carOos;
+    final ObjectInputStream carOis;
+    final ObjectOutputStream roomOos;
+    final ObjectInputStream roomOis;
 
     public MiddlewareRequestHandler(Socket customerClient, Socket flightClient, Socket carClient, Socket roomClient) throws IOException {
         this.customerClient = customerClient;
-        this.customerIn = new BufferedReader(new InputStreamReader(customerClient.getInputStream()));
-        this.customerOut = new PrintWriter(customerClient.getOutputStream(), true);
         this.flightClient = flightClient;
-        this.flightIn = new BufferedReader(new InputStreamReader(flightClient.getInputStream()));
-        this.flightOut = new PrintWriter(flightClient.getOutputStream(), true);
         this.carClient = carClient;
-        this.carIn = new BufferedReader(new InputStreamReader(carClient.getInputStream()));
-        this.carOut = new PrintWriter(carClient.getOutputStream(), true);
         this.roomClient = roomClient;
-        this.roomIn = new BufferedReader(new InputStreamReader(roomClient.getInputStream()));
-        this.roomOut = new PrintWriter(roomClient.getOutputStream(), true);
+
+        this.flightOos = new ObjectOutputStream(flightClient.getOutputStream());
+        this.flightOis = new ObjectInputStream(flightClient.getInputStream());
+        this.carOos = new ObjectOutputStream(carClient.getOutputStream());
+        this.carOis = new ObjectInputStream(carClient.getInputStream());
+        this.roomOos = new ObjectOutputStream(roomClient.getOutputStream());
+        this.roomOis = new ObjectInputStream(roomClient.getInputStream());
+
     }
 
-    public Response handle(Request req) throws IOException, ClassNotFoundException{
+    public Response handle(Request req) throws IOException, ClassNotFoundException {
         System.out.println("REQUEST: " + req.getMessage());
-        Response response = new Response("HI");
+        this.flightOos.writeObject(req);
+        Response response = (Response) this.flightOis.readObject();
+        System.out.println("RESPONSE" + response.getMessage());
         return response;
     }
 }
