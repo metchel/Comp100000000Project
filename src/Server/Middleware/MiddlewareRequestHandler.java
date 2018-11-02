daff43aa665207db09b139c05e655e97cc49c58b
metchel <matthew.etchells@mail.mcgill.ca>
2018-11-02 18:01:09 -0400

    just gotta do bundles now.

--- src/Server/Middleware/MiddlewareRequestHandler.java
+++ src/Server/Middleware/MiddlewareRequestHandler.java
@@ -1,10 +1,17 @@
 package Server.Middleware;
 
 import Server.Network.Request;
+import Server.Network.RequestData;
 import Server.Network.Response;
+import Server.ResourceManager.SocketResourceManager;
+import Server.Common.Command;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Map;
+import java.util.Queue;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import Server.Sockets.RequestHandler;
 
@@ -13,23 +20,143 @@ public class MiddlewareRequestHandler implements RequestHandler {
     final MiddlewareClient carClient;
     final MiddlewareClient roomClient;
 
+    final SocketResourceManager customerResourceManager;
+    final Map<String, Queue<Request>> transactionMap;
+
     public MiddlewareRequestHandler(MiddlewareClient flightClient, MiddlewareClient carClient, MiddlewareClient roomClient) throws IOException, ClassNotFoundException {
         this.flightClient = flightClient;
         this.carClient = carClient;
         this.roomClient = roomClient;
+        this.customerResourceManager = new SocketResourceManager("Customers");
+        this.transactionMap = new HashMap<String, Queue<Request>>();
+        this.transactionMap.put("FLIGHTS", new LinkedList<Request>());
+        this.transactionMap.put("CARS", new LinkedList<Request>());
+        this.transactionMap.put("ROOMS", new LinkedList<Request>());
+        this.transactionMap.put("CUSTOMERS", new LinkedList<Request>());
     }
 
     public Response handle(Request req) throws IOException, ClassNotFoundException {
         System.out.println("REQUEST: " + req.toString());
-        this.flightClient.send(req);
-        this.carClient.send(req);
-        this.roomClient.send(req);
-        Response response = (Response) this.flightClient.receive();
-        Response response2 = (Response) this.carClient.receive();
-        Response response3 = (Response) this.roomClient.receive();
+        requestMapper(req);
+
+        Response response = null;
+        while(!this.transactionMap.get("FLIGHTS").isEmpty()) {
+            Request request = this.transactionMap.get("FLIGHTS").remove();
+            this.flightClient.send(request);
+            response = this.flightClient.receive();
+        }
+
+        while(!this.transactionMap.get("CARS").isEmpty()) {
+            Request request = this.transactionMap.get("CARS").remove();
+            this.carClient.send(request);
+            response = this.carClient.receive();
+        }
+
+        while(!this.transactionMap.get("ROOMS").isEmpty()) {
+            Request request = this.transactionMap.get("ROOMS").remove();
+            this.roomClient.send(request);
+            response = roomClient.receive();
+        }
         System.out.println("RESPONSE: " + response.toString());
         return response;
     }
 
+    public void requestMapper(Request request) {
+        RequestData data = (RequestData) request.getData();
+        Command command = data.getCommand();
 
+        switch (command)
+        {
+            case Help:
+            {
+                break;
+            }
+            case AddFlight: {
+                this.transactionMap.get("FLIGHTS").add(request);
+                break;
+            }
+
+            case AddCars: {
+                this.transactionMap.get("CARS").add(request);
+                break;
+            }
+
+            case AddRooms: {
+                this.transactionMap.get("ROOMS").add(request);
+                break;
+            }
+
+            case AddCustomer: {
+                this.transactionMap.get("CUSTOMERS").add(request);
+                break;
+            }
+            case AddCustomerID: {
+                this.transactionMap.get("CUSTOMERS").add(request);
+                break;
+            }
+            case DeleteFlight: {
+                this.transactionMap.get("FLIGHTS").add(request);
+                break;
+            }
+            case DeleteCars: {
+                this.transactionMap.get("CARS").add(request);
+                break;
+            }
+            case DeleteRooms: {
+                this.transactionMap.get("ROOMS").add(request);
+                break;
+            }
+            case DeleteCustomer: {
+                this.transactionMap.get("CUSTOMERS").add(request);
+                break;
+            }
+            case QueryFlight: {
+                this.transactionMap.get("FLIGHTS").add(request);
+                break;
+            }
+            case QueryCars: {
+                this.transactionMap.get("CARS").add(request);
+                break;
+            }
+            case QueryRooms: {
+                this.transactionMap.get("CARS").add(request);
+                break;
+            }
+            case QueryCustomer: {
+                this.transactionMap.get("CUSTOMERS").add(request);
+                break;
+            }
+            case QueryFlightPrice: {
+                this.transactionMap.get("FLIGHTS").add(request);
+                break;
+            }
+            case QueryCarsPrice: {
+                this.transactionMap.get("CARS").add(request);
+                break;
+            }
+            case QueryRoomsPrice: {
+                this.transactionMap.get("ROOMS").add(request);
+                break;
+            }
+            case ReserveFlight: {
+                this.transactionMap.get("FLIGHTS").add(request);
+                break;
+            }
+            case ReserveCar: {
+                this.transactionMap.get("CARS").add(request);
+                break;
+            }
+            case ReserveRoom: {
+                this.transactionMap.get("ROOMS").add(request);
+                break;
+            }
+            case Bundle: {
+                this.transactionMap.get("CUSTOMERS").add(request);
+                this.transactionMap.get("FLIGHTS").add(request);
+                this.transactionMap.get("CARS").add(request);
+                this.transactionMap.get("ROOMS").add(request);
+                break;
+            }
+        }
+    }
 }
--- src/Server/Sockets/ServerRequestHandler.java
+++ src/Server/Sockets/ServerRequestHandler.java
@@ -20,42 +20,151 @@ public class ServerRequestHandler implements RequestHandler {
 
     public Response handle(Request req) throws IOException, ClassNotFoundException {
         Map<String, Object> arguments = ((RequestData)req.getData()).getCommandArgs();
-        Command cmd = ((RequestData)req.getData()).getCommand();
-        Integer xId = req.getData().getXId();
+        RequestData data = req.getData();
+        Command cmd = data.getCommand();
+        Integer xId = data.getXId();
 
         System.out.println(req.toString());
 
-        Boolean resStatus = execute(xId, cmd, arguments);
+        Object result = execute(xId, cmd, arguments);
+        Boolean resStatus = null;
+        String message = null;
+        if (result instanceof Integer) {
+            resStatus = new Boolean(true);
+            message = result.toString();
+        }
+        if (result instanceof Boolean) {
+            resStatus = (Boolean)result;
+            message = result.toString();
+        }
 
         Response response = new Response();
         response.addCurrentTimeStamp()
             .addStatus(resStatus)
-            .addMessage("IDK");
+            .addMessage(message);
 
         System.out.println(response.toString());
 
         return response;
     }
 
-    public Boolean execute(Integer xId, Command cmd, Map<String, Object> arguments) throws IOException {
+    public Object execute(Integer xId, Command cmd, Map<String, Object> arguments) throws IOException {
         switch (cmd) {
             case Help: {
-                break;
+                return new Boolean(false);
             }
 
             case AddFlight: {
 
-                int flightNum = ((Integer)arguments.get("flightNum")).intValue();
-                int flightSeats = ((Integer)arguments.get("flightSeats")).intValue();
-                int flightPrice = ((Integer)arguments.get("flightPrice")).intValue();
+                Integer flightNum = (Integer)arguments.get("flightNum");
+                Integer flightSeats = (Integer)arguments.get("flightSeats");
+                Integer flightPrice = (Integer)arguments.get("flightPrice");
 
-                if (resourceManager.addFlight(xId, flightNum, flightSeats, flightPrice)) {
-                    System.out.println("Flight added");
-                    return new Boolean(true);
-                } else {
-                    System.out.println("Flight could not be added");
-                    return new Boolean(false);
-                }
+                return new Boolean(resourceManager.addFlight(xId.intValue(), flightNum.intValue(), flightSeats.intValue(), flightPrice.intValue()));
+            }
+
+            case AddCars: {
+                String carLoc = (String)arguments.get("carLoc");
+                Integer numCars = (Integer)arguments.get("numCars");
+                Integer carPrice = (Integer)arguments.get("carPrice");
+
+                return new Boolean(resourceManager.addCars(xId.intValue(), carLoc, numCars.intValue(), carPrice.intValue()));
+            }
+
+            case AddRooms: {
+                String roomLoc = (String)arguments.get("roomLoc");
+                Integer numRooms = (Integer)arguments.get("numRooms");
+                Integer roomPrice = (Integer)arguments.get("roomPrice");
+
+                return new Boolean(resourceManager.addRooms(xId, roomLoc, numRooms.intValue(), roomPrice.intValue()));
+            }
+
+            case AddCustomer: {
+                return new Integer(resourceManager.newCustomer(xId));
+            }
+
+            case AddCustomerID: {
+                Integer cId = (Integer)arguments.get("cId");
+                return new Boolean(resourceManager.newCustomer(xId, cId.intValue()));
+            }
+
+            case DeleteFlight: {
+                Integer flightNum = (Integer)arguments.get("flightNum");
+                return new Boolean(resourceManager.deleteFlight(xId, flightNum.intValue()));
+            }
+
+            case DeleteCars: {
+                String carLoc = (String)arguments.get("carLoc");
+                return new Boolean(resourceManager.deleteCars(xId, carLoc));
+            }
+
+            case DeleteRooms: {
+                String roomLoc = (String)arguments.get("roomLoc");
+                return new Boolean(resourceManager.deleteRooms(xId, roomLoc));
+            }
+
+            case QueryFlight: {
+                Integer flightNum = (Integer)arguments.get("flightNum");
+                return new Integer(resourceManager.queryFlight(xId, flightNum.intValue()));
+            }
+
+            case QueryCars: {
+                String carLoc = (String)arguments.get("carLoc");
+                return new Integer(resourceManager.queryCars(xId, carLoc));
+            }
+
+            case QueryRooms: {
+                String roomLoc = (String)arguments.get("roomLoc");
+                return new Integer(resourceManager.queryRooms(xId, roomLoc));
+            }
+
+            case QueryFlightPrice: {
+                Integer flightNum = (Integer)arguments.get("flightNum");
+                return new Integer(resourceManager.queryFlightPrice(xId, flightNum.intValue()));
+            }
+
+            case QueryCarsPrice: {
+                String carLoc = (String)arguments.get("carLoc");
+                return new Integer(resourceManager.queryCarsPrice(xId, carLoc));
+            }
+
+            case QueryRoomsPrice: {
+                String roomLoc = (String)arguments.get("roomLoc");
+                return new Integer(resourceManager.queryRoomsPrice(xId, roomLoc));
+            }
+
+            case QueryCustomer: {
+                return new Boolean(false);
+            }
+
+            case DeleteCustomer: {
+                return new Boolean(false);
+            }
+
+            case ReserveFlight: {
+                Integer cId = (Integer)arguments.get("cId");
+                Integer flightNum = (Integer)arguments.get("flightNum");
+                return new Boolean(resourceManager.reserveFlight(xId, cId, flightNum));
+            }
+
+            case ReserveCar: {
+                Integer cId = (Integer)arguments.get("cId");
+                String carLoc = (String)arguments.get("carLoc");
+                return new Boolean(resourceManager.reserveCar(xId, cId, carLoc));
+            }
+
+            case ReserveRoom: {
+                Integer cId = (Integer)arguments.get("cId");
+                String roomLoc = (String)arguments.get("roomLoc");
+                return new Boolean(resourceManager.reserveRoom(xId, cId, roomLoc));
+            }
+
+            case Bundle: {
+                return new Boolean(false);
+            }
+
+            case Quit: {
+                return new Boolean(false);
             }
         }
 
